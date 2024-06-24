b46511039e012a4fd49fb6e010304844ee3a9864
pramodkp72 <pramod.kp72@gmail.com>
2024-06-24 16:53:12 +0530

    remove web driver

--- app/src/main/java/testing/aasaan/util/WebDriverUtil.java
+++ app/src/main/java/testing/aasaan/util/WebDriverUtil.java
@@ -2,15 +2,22 @@ package aasaan.testing.util;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverUtil {
     private static WebDriver driver;
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            System.out.println("Setting up WebDriver");
-            System.setProperty("webdriver.chrome.driver", "/Users/pramodkalidindi/Downloads/chromedriver-mac-x64/chromedriver");
-            driver = new ChromeDriver();
+            // To run chrome options in headless
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+            /////////////////////////////////////////////////
+            // To run in normal i.e. non-headless mode  
+            // driver = new ChromeDriver();
         }
         return driver;
     }
