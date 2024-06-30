8da8b0ad2e36163dd5205ef3aefbf0edf533259b
pramodkp72 <pramod.kp72@gmail.com>
2024-06-30 15:45:20 +0530

    remove web driver manager and install chrome on docker

--- app/src/main/java/testing/aasaan/util/WebDriverUtil.java
+++ app/src/main/java/testing/aasaan/util/WebDriverUtil.java
@@ -6,20 +6,30 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 public class WebDriverUtil {
-    private static WebDriver driver; // This driver was shadowed!
+    private static WebDriver driver;
 
     public static WebDriver getDriver() {
-        if (driver == null) { 
-            WebDriverManager.chromedriver().setup(); 
+        if (driver == null) {
 
+        //     // 1. Setup WebDriverManager (downloads if needed)
+        // WebDriverManager.chromedriver().setup(); 
+
+        // // 2. (Optional) Headless Chrome Configuration
+        // ChromeOptions options = new ChromeOptions();
+        // options.addArguments("--headless=new");
+        // options.addArguments("--disable-gpu"); // Might be needed
+
+        // // 3. Instantiate your WebDriver
+        // WebDriver driver = new ChromeDriver(options); // Use options if headless
+            // To run chrome options in headless
             ChromeOptions options = new ChromeOptions();
-            options.addArguments("--headless=new");
-            options.addArguments("--disable-gpu"); 
-
-            // Assign to the class-level driver variable
-            driver = new ChromeDriver(options);  
-
-            // ... rest of your initialization code
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+            driver = new ChromeDriver(options);
+            /////////////////////////////////////////////////
+            // To run in normal i.e. non-headless mode  
+            // driver = new ChromeDriver();
         }
         return driver;
     }
